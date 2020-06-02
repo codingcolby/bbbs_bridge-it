e14d06a8899335e7a6a33192853326d50a42a9b3
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-02 15:20:20 -0500

    added style to mapsearch page

--- src/pages/MapSearchPage/MapSearchPage.js
+++ src/pages/MapSearchPage/MapSearchPage.js
@@ -2,9 +2,23 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import Map from "../../components/Map/Map";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  Typography,
+  withStyles,
+  createStyles,
+} from "@material-ui/core";
+
+const customStyles = (theme) =>
+  createStyles({
+    btn: {},
+  });
 
 class MapSearchPage extends Component {
   render() {
+    const { classes } = this.props;
     return (
       <div>
         <h2>Map Search Page</h2>
@@ -12,8 +26,16 @@ class MapSearchPage extends Component {
         <div>
           <Map />
         </div>
+
+        <Checkbox></Checkbox>
+
+        <Button className={classes.btn} variant="outlined">
+          Search By Name
+        </Button>
       </div>
     );
   }
 }
-export default connect(mapStoreToProps)(MapSearchPage);
+export default withStyles(customStyles)(
+  connect(mapStoreToProps)(MapSearchPage)
+);
