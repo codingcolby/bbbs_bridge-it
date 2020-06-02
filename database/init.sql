6289c57bb59b7a0114cc1361d66a488ad6772838
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-02 09:56:40 -0500

    added database queries

--- database/init.sql
+++ database/init.sql
@@ -2,8 +2,81 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "profile"
+(
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "profile_type" int,
+    "first_name" varchar(64),
+    "last_name" varchar(64),
+    "sex" int,
+    "dob_or_age" text,
+    "race" text,
+    "address" text,
+    "latitude" double precision,
+    "longitude" double precision,
+    "ems" text,
+    "summary" text,
+    "preference" text,
+    "interest" text,
+    "l_parent" text,
+    "l_parent_relationship_to_child" text,
+    "b_employer" text,
+    "b_occupation" text,
+    "b_marital_status" text
 );
+
+CREATE TABLE "status"
+(
+    "id" SERIAL PRIMARY KEY,
+    "big_id" int,
+    "little_id" int,
+    "match" boolean DEFAULT FALSE,
+    "review" int DEFAULT NULL,
+    "comment" text
+);
+
+CREATE TABLE "user"
+(
+    "id" SERIAL PRIMARY KEY,
+    "email" varchar(128),
+    "password" varchar(1000),
+    "admin" boolean DEFAULT FALSE
+);
+
+CREATE TABLE "review_type"
+(
+    "id" SERIAL PRIMARY KEY,
+    "type" text
+);
+
+CREATE TABLE "sex_type"
+(
+    "id" SERIAL PRIMARY KEY,
+    "type" text
+);
+
+CREATE TABLE "profile_type"
+(
+    "id" SERIAL PRIMARY KEY,
+    "type" text
+);
+
+INSERT INTO "review_type"
+    ("type")
+VALUES
+    ('unlikely'),
+    ('maybe'),
+    ('likely');
+
+INSERT INTO "sex_type"
+    ("type")
+VALUES
+    ('female'),
+    ('male');
+
+INSERT INTO "profile_type"
+    ("type")
+VALUES
+    ('big'),
+    ('little'),
+    ('couple');
