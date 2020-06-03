225ee6164750e696950dd434f46a75d9e3c6706a
cchelton <59346941+cchelton@users.noreply.github.com>
2020-06-03 10:59:09 -0500

    adds buttons

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,14 +1,26 @@
 import React from "react";
 import PersonCard from "../../components/PersonCard/PersonCard";
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, Button, IconButton } from "@material-ui/core";
+import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
+import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
-    justifyContent: "space-evenly",
+    justifyContent: "center",
   },
   column: {
     display: "inline-block",
+  },
+  btnCol: {
+    display: "inline-flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingLeft: "15px",
+    paddingRight: "15px",
+  },
+  btn: {
+    paddingTop: "15px",
   },
 }));
 
@@ -18,6 +30,16 @@ function ProfilePage(props) {
     <div className={classes.root}>
       <div className={classes.column}>
         <PersonCard />
+        <br />
+        <Button className={classes.btn}>Map View</Button>
+      </div>
+      <div className={classes.btnCol}>
+        <IconButton>
+          <ArrowUpwardIcon />
+        </IconButton>
+        <IconButton>
+          <ArrowDownwardIcon />
+        </IconButton>
       </div>
       <div className={classes.column}>
         <PersonCard />
