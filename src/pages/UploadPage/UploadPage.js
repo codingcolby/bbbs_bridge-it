d5caf8f82776a86fd782278a3b4f3cc2896ecb1a
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-03 09:17:54 -0500

    setup of upload page dropdown menu

--- src/pages/UploadPage/UploadPage.js
+++ src/pages/UploadPage/UploadPage.js
@@ -1,16 +1,44 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import mapStoreToProps from "../../redux/mapStoreToProps";
-import { Button, Menu, MenuItem } from "@material-ui/core";
+import React, { useState } from "react";
+import { Menu, MenuItem, Button } from "@material-ui/core";
 
-class UploadPage extends Component {
-  render() {
-    return (
+function UploadPage(props) {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
       <div>
-        <h2>Upload Page</h2>
+        <Button onClick={handleClick} variant="outlined">
+          Upload Big or Little
+        </Button>
+        <Menu
+          getContentAnchorEl={null}
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)} // open state of menu
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuItem>Big</MenuItem>
+          <MenuItem>Little</MenuItem>
+        </Menu>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-export default connect(mapStoreToProps)(UploadPage);
+export default UploadPage;
