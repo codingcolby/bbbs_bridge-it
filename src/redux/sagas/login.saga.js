98bc53b7341de16063a9e5cf041c4a9114f1c829
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-04 11:07:52 -0500

    resolved login issue

--- src/redux/sagas/login.saga.js
+++ src/redux/sagas/login.saga.js
@@ -15,7 +15,14 @@ function* loginUser(action) {
     // send the action.payload as the body
     // the config includes credentials which
     // allow the server session to recognize the user
-    yield axios.post("/api/user/login", action.payload, config);
+    yield axios.post(
+      "/api/user/login",
+      {
+        username: action.payload.email,
+        password: action.payload.password,
+      },
+      config
+    );
 
     // after the user has logged in
     // get the user information from the server
