2ded5e9e0f882be8c31b7e2fc63b4689dd6d6087
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-04 13:23:15 -0500

    styled search bar

--- src/pages/SearchPage/SearchPage.js
+++ src/pages/SearchPage/SearchPage.js
@@ -3,16 +3,57 @@ import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import {
   Button,
-  TextField,
-  Typography,
   withStyles,
   createStyles,
+  InputBase,
+  fade,
 } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 const customStyles = (theme) =>
   createStyles({
-    btn: {},
-    input: {},
+    root: {
+      textAlign: "center",
+    },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.2),
+      },
+      marginLeft: 0,
+      marginBottom: "200px",
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(1),
+        width: "auto",
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "inline",
+    },
+    inputRoot: {
+      color: "inherit",
+    },
+    inputInput: {
+      border: "solid 2px black",
+      padding: theme.spacing(1, 1, 1, 10),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: "30ch",
+        "&:focus": {
+          width: "40ch",
+        },
+      },
+    },
   });
 
 class SearchPage extends Component {
@@ -20,21 +61,21 @@ class SearchPage extends Component {
     const { classes } = this.props;
 
     return (
-      <div>
+      <div className={classes.root}>
         <h2>Search Page</h2>
         <br />
-        <div></div>
-        <div>
-          <form className={classes.input} noValidate autoComplete="off">
-            <TextField id="standard-basic" label="Search by Full Name" />
-            <Button className={classes.btn} variant="outlined">
-              Search
-            </Button>
-          </form>
-        </div>
-        <br />
-        <div>
-          <li>Big or Little Name</li>
+
+        <div className={classes.search}>
+          <SearchIcon className={classes.searchIcon} />
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ "aria-label": "search" }}
+            // onChange={this.changeSearch}
+          />
         </div>
         <Button className={classes.btn} variant="outlined">
           Search By Map
