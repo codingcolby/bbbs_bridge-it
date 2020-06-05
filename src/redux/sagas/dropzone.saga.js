0d1404b5a6cc20d30e1ed801bf35dd917045e4ce
cchelton <59346941+cchelton@users.noreply.github.com>
2020-06-04 22:12:59 -0500

    updated expected payload

--- src/redux/sagas/dropzone.saga.js
+++ src/redux/sagas/dropzone.saga.js
@@ -4,7 +4,7 @@ import axios from "axios";
 /**
  * Send pdf to the server.
  *
- * Expected payload: { pdfUrl: str }
+ * Expected payload: { pdfUrl: str, docType: num }
  */
 function* savePdf(action) {
   try {
