4f41e9596e6afaa45b879ae8fbcaa1c217c59445
allthethings <learn2code@respectdesfonds.com>
2020-06-05 14:51:20 -0500

    bottom button for alternate page views are now fully functional

--- src/pages/MapSearchPage/MapSearchPage.js
+++ src/pages/MapSearchPage/MapSearchPage.js
@@ -4,59 +4,66 @@ import mapStoreToProps from "../../redux/mapStoreToProps";
 import Map from "../../components/Map/Map";
 
 import {
-  Button,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  withStyles,
-  createStyles,
+	Button,
+	Checkbox,
+	FormControlLabel,
+	FormGroup,
+	withStyles,
+	createStyles,
 } from "@material-ui/core";
 
 const customStyles = (theme) =>
-  createStyles({
-    root: {
-      textAlign: "center",
-    },
-    position: {
-      display: "inline-block",
-      padding: "15px",
-    },
-  });
+	createStyles({
+		root: {
+			textAlign: "center",
+		},
+		position: {
+			display: "inline-block",
+			padding: "15px",
+		},
+	});
 
 class MapSearchPage extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.root}>
-        <h1>All Unmatched Bigs and Littles</h1>
-        <br />
-        <div>
-          <Map />
-        </div>
-        <div className={classes.position}>
-          <FormGroup row>
-            <FormControlLabel
-              control={<Checkbox name="checkedFemale" />}
-              label="Female"
-            />
-            <FormControlLabel
-              control={<Checkbox name="checkedMale" />}
-              label="Male"
-            />
-            <FormControlLabel
-              control={<Checkbox name="checkedCouple" />}
-              label="Couple"
-            />
-          </FormGroup>
-        </div>
-        <div>
-          <Button variant="outlined">Search By Name</Button>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		const { classes } = this.props;
+
+		const handleClick = () => {
+			this.props.history.push("/search");
+		};
+
+		return (
+			<div className={classes.root}>
+				<h1>All Unmatched Bigs and Littles</h1>
+				<br />
+				<div>
+					<Map />
+				</div>
+				<div className={classes.position}>
+					<FormGroup row>
+						<FormControlLabel
+							control={<Checkbox name="checkedFemale" />}
+							label="Female"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="checkedMale" />}
+							label="Male"
+						/>
+						<FormControlLabel
+							control={<Checkbox name="checkedCouple" />}
+							label="Couple"
+						/>
+					</FormGroup>
+				</div>
+				<div>
+					<Button variant="outlined" onClick={handleClick}>
+						Search By Name
+					</Button>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withStyles(customStyles)(
-  connect(mapStoreToProps)(MapSearchPage)
+	connect(mapStoreToProps)(MapSearchPage)
 );
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,51 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 import PersonCard from "../../components/PersonCard/PersonCard";
-import { makeStyles, Button, IconButton } from "@material-ui/core";
+import {
+	withStyles,
+	createStyles,
+	makeStyles,
+	Button,
+	IconButton,
+} from "@material-ui/core";
 import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
 import ArrowDownwardIcon from "@material-ui/icons/ArrowDownward";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "center",
-  },
-  column: {
-    display: "inline-block",
-  },
-  btnCol: {
-    display: "inline-flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    paddingLeft: "15px",
-    paddingRight: "15px",
-  },
-  btn: {
-    paddingTop: "15px",
-  },
-}));
+const customStyles = (theme) =>
+	createStyles({
+		root: {
+			display: "flex",
+			justifyContent: "center",
+		},
+		column: {
+			display: "inline-block",
+		},
+		btnCol: {
+			display: "inline-flex",
+			flexDirection: "column",
+			justifyContent: "center",
+			paddingLeft: "15px",
+			paddingRight: "15px",
+		},
+	});
 
-function ProfilePage(props) {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <div className={classes.column}>
-        <PersonCard />
-        <br />
-        <Button className={classes.btn}>Map View</Button>
-      </div>
-      <div className={classes.btnCol}>
-        <IconButton>
-          <ArrowUpwardIcon />
-        </IconButton>
-        <IconButton>
-          <ArrowDownwardIcon />
-        </IconButton>
-      </div>
-      <div className={classes.column}>
-        <PersonCard />
-      </div>
-    </div>
-  );
+class ProfilePage extends Component {
+	render() {
+		const { classes } = this.props;
+
+		const handleClick = () => {
+			this.props.history.push("/list");
+		};
+
+		return (
+			<div className={classes.root}>
+				<div className={classes.column}>
+					<PersonCard />
+					<br />
+					<Button variant="outlined" onClick={handleClick}>
+						Map List View
+					</Button>
+				</div>
+				<div className={classes.btnCol}>
+					<IconButton>
+						<ArrowUpwardIcon />
+					</IconButton>
+					<IconButton>
+						<ArrowDownwardIcon />
+					</IconButton>
+				</div>
+				<div className={classes.column}>
+					<PersonCard />
+				</div>
+			</div>
+		);
+	}
 }
-
-export default ProfilePage;
+export default withStyles(customStyles)(connect(mapStoreToProps)(ProfilePage));
