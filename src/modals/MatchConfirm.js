dd7bae89eaa5517e32fbabbca94f99d408b9a4c6
allthethings <learn2code@respectdesfonds.com>
2020-06-08 10:18:53 -0500

    troubleshooting react hook error - in progress

--- src/modals/MatchConfirm.js
+++ src/modals/MatchConfirm.js
@@ -2,6 +2,17 @@ import React from "react";
 import Modal from "@material-ui/core/Modal";
 import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+	paper: {
+		position: "absolute",
+		width: 400,
+		backgroundColor: theme.palette.background.paper,
+		border: "2px solid #000",
+		boxShadow: theme.shadows[5],
+		padding: theme.spacing(2, 4, 3),
+	},
+}));
+
 function rand() {
 	return Math.round(Math.random() * 20) - 10;
 }
@@ -17,54 +28,44 @@ function getModalStyle() {
 	};
 }
 
-const useStyles = makeStyles((theme) => ({
-	paper: {
-		position: "absolute",
-		width: 400,
-		backgroundColor: theme.palette.background.paper,
-		border: "2px solid #000",
-		boxShadow: theme.shadows[5],
-		padding: theme.spacing(2, 4, 3),
-	},
-}));
+const MatchConfirm = () => {
+	const classes = useStyles();
+	// getModalStyle is not a pure function, we roll the style only on the first render
+	const [modalStyle] = React.useState(getModalStyle);
+	const [open, setOpen] = React.useState(false);
 
-const classes = useStyles();
-// getModalStyle is not a pure function, we roll the style only on the first render
-const [modalStyle] = React.useState(getModalStyle);
-const [open, setOpen] = React.useState(false);
+	const handleYes = () => {
+		// Clicking "Yes"...
+	};
+	const handleNo = () => {
+		// Clicking "No" ...
+	};
 
-const handleYes = () => {
-	// Clicking "Yes"...
+	const handleClose = () => {
+		setOpen(false);
+	};
+	return (
+		<div>
+			<Modal
+				open={open}
+				onClose={handleClose}
+				aria-labelledby="simple-modal-title"
+				aria-describedby="simple-modal-description">
+				<div style={modalStyle} className={classes.paper}>
+					<h2 id="simple-modal-title">Are you sure?</h2>
+					<p id="simple-modal-description">
+						Are you sure you want to perform this action?
+					</p>
+				</div>
+				<button type="button" onClick={handleYes}>
+					Yes
+				</button>
+				<button type="button" onClick={handleNo}>
+					No
+				</button>
+			</Modal>
+		</div>
+	);
 };
-const handleNo = () => {
-	// Clicking "No" ...
-};
-
-const handleClose = () => {
-	setOpen(false);
-};
-
-const MatchConfirm = () => (
-	<div>
-		<Modal
-			open={open}
-			onClose={handleClose}
-			aria-labelledby="simple-modal-title"
-			aria-describedby="simple-modal-description">
-			<div style={modalStyle} className={classes.paper}>
-				<h2 id="simple-modal-title">Are you sure?</h2>
-				<p id="simple-modal-description">
-					Are you sure you want to perform this action?
-				</p>
-			</div>
-			<button type="button" onClick={handleYes}>
-				Yes
-			</button>
-			<button type="button" onClick={handleNo}>
-				No
-			</button>
-		</Modal>
-	</div>
-);
 
 export default MatchConfirm;
