3ad0f6b4fdf5e1296eec45bc2bdb3f8f699fb557
allthethings <learn2code@respectdesfonds.com>
2020-06-08 16:28:06 -0500

    added ignore for TS

--- server/routes/user.router.ts
+++ server/routes/user.router.ts
@@ -8,53 +8,58 @@ import { encryptPassword } from "../modules/encryption";
 const router: express.Router = express.Router();
 
 router.get("/", rejectUnauthenticated, (req: Request, res: Response): void => {
-  console.log("USER", req.user);
-  res.send(req.user);
+	//@ts-ignore
+	console.log("USER", req.user);
+	//@ts-ignore
+	res.send(req.user);
 });
 
 router.get("/profiles", (req: Request, res: Response): void => {
-  //get all profiles
-  const queryText: string = `SELECT * FROM "profile";`;
+	//get all profiles
+	const queryText: string = `SELECT * FROM "profile";`;
 
-  pool
-    .query(queryText)
-    .then((response) => {
-      res.send(response.rows);
-    })
-    .catch((err) => {
-      console.log(`Error getting profiles from database: ${err}`);
-      res.sendStatus(500);
-    });
+	pool
+		.query(queryText)
+		.then((response) => {
+			res.send(response.rows);
+		})
+		.catch((err) => {
+			console.log(`Error getting profiles from database: ${err}`);
+			res.sendStatus(500);
+		});
 });
 
 router.post(
-  "/register",
-  (req: Request, res: Response, next: express.NextFunction): void => {
-    const email: string | null = <string>req.body.email;
-    const password: string | null = encryptPassword(req.body.password);
+	"/register",
+	(req: Request, res: Response, next: express.NextFunction): void => {
+		//@ts-ignore
+		const email: string | null = <string>req.body.email;
+		//@ts-ignore
+		const password: string | null = encryptPassword(req.body.password);
 
-    const queryText: string = `INSERT INTO "user" (email, password) VALUES ($1, $2) RETURNING id`;
-    pool
-      .query(queryText, [email, password])
-      .then(() => res.sendStatus(201))
-      .catch((err) => {
-        console.log(`Error saving user to database: ${err}`);
-        res.sendStatus(500);
-      });
-  }
+		const queryText: string = `INSERT INTO "user" (email, password) VALUES ($1, $2) RETURNING id`;
+		pool
+			.query(queryText, [email, password])
+			.then(() => res.sendStatus(201))
+			.catch((err) => {
+				console.log(`Error saving user to database: ${err}`);
+				res.sendStatus(500);
+			});
+	}
 );
 
 router.post(
-  "/login",
-  userStrategy.authenticate("local"),
-  (req: Request, res: Response): void => {
-    res.sendStatus(200);
-  }
+	"/login",
+	userStrategy.authenticate("local"),
+	(req: Request, res: Response): void => {
+		res.sendStatus(200);
+	}
 );
 
 router.post("/logout", (req: Request, res: Response): void => {
-  req.logout();
-  res.sendStatus(200);
+	//@ts-ignore
+	req.logout();
+	res.sendStatus(200);
 });
 
 export default router;
