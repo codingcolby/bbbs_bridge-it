e7c27d3849a6ed6743c5066cc7036c017fec5100
allthethings <learn2code@respectdesfonds.com>
2020-06-10 00:39:20 -0500

    created request handling for Match Table page with addition of reducer, saga, router, and update to the server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import sessionMiddleware from "./modules/session-middleware";
 import passport from "./strategies/user.strategy";
 import userRouter from "./routes/user.router";
+import matchtableRouter from "./routes/matchtable.router";
 
 const uploaderS3Router = require("react-s3-uploader/s3router"); // no @types for this module
 
@@ -23,14 +24,15 @@ app.use(passport.session());
 
 /* Routes */
 app.use("/api/user", userRouter);
+app.use("/api/table", matchtableRouter);
 app.use(
-  "/s3",
-  uploaderS3Router({
-    bucket: "bridge-it-bucket", // required
-    region: "us-east-2", // optional
-    headers: { "Access-Control-Allow-Origin": "*" }, // optional
-    ACL: "public-read", // this is the default - set to `public-read` to let anyone view uploads
-  }) // PRIME!!! we'll need to make it so this app is the only app that may access the bucket
+	"/s3",
+	uploaderS3Router({
+		bucket: "bridge-it-bucket", // required
+		region: "us-east-2", // optional
+		headers: { "Access-Control-Allow-Origin": "*" }, // optional
+		ACL: "public-read", // this is the default - set to `public-read` to let anyone view uploads
+	}) // PRIME!!! we'll need to make it so this app is the only app that may access the bucket
 );
 
 // Serve static files
@@ -41,7 +43,7 @@ const PORT: number | string = process.env.PORT || 5000;
 
 /** Listen * */
 app.listen(PORT, (): void => {
-  console.log(`So awesome. Much wow. Listening on port: ${PORT}`);
+	console.log(`So awesome. Much wow. Listening on port: ${PORT}`);
 });
 
 export default app;
---
+++ src/redux/reducers/matchtable.reducer.js
@@ -0,0 +1,15 @@
+const matchtableReducer = (state = {}, action) => {
+	switch (action.type) {
+		//set matchtableReducer with select profiles returned from server
+		case "SET_TABLE":
+			return action.payload;
+		case "UNSET_TABLE":
+			return {};
+		default:
+			return state;
+	}
+};
+
+// user will be on the redux state at:
+// state.user
+export default matchtableReducer;
