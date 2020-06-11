8dc90d85c13020f1d09a51b3a0410898fefe709f
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-11 11:30:51 -0500

    added scroll

--- src/pages/MapListPage/MapListPage.js
+++ src/pages/MapListPage/MapListPage.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import Map from "../../components/Map/Map";
+import * as Scroll from "react-scroll";
+import {
+  Link,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller,
+} from "react-scroll";
+
 import {
   Button,
   Card,
@@ -82,6 +92,7 @@ class MapListPage extends Component {
                 valueLabelDisplay="auto"
                 marks={marks}
               />
+
               <Card>
                 <CardContent>
                   <RadioGroup>
@@ -114,27 +125,38 @@ class MapListPage extends Component {
                   );
                 })}
               </div>
-              <div>
-                {profilesLittlesFilter.map((item, index) => {
-                  return (
-                    <Card key={index}>
-                      <CardContent>
-                        <div>
-                          <h3>{item.first_name + " " + item.last_name}</h3>
-                          <h4>
-                            {"Age: " +
-                              item.dob_or_age +
-                              " " +
-                              "Race: " +
-                              item.race}
-                          </h4>
-                          <p>{item.summary}</p>
-                        </div>
-                      </CardContent>
-                    </Card>
-                  );
-                })}
-              </div>
+              <Element
+                name="littles-list"
+                className="element"
+                style={{
+                  position: "relative",
+                  height: "540px",
+                  overflow: "scroll",
+                  marginBottom: "100px",
+                }}
+              >
+                <div>
+                  {profilesLittlesFilter.map((item, index) => {
+                    return (
+                      <Card key={index}>
+                        <CardContent>
+                          <div>
+                            <h3>{item.first_name + " " + item.last_name}</h3>
+                            <h4>
+                              {"Age: " +
+                                item.dob_or_age +
+                                " " +
+                                "Race: " +
+                                item.race}
+                            </h4>
+                            <p>{item.summary}</p>
+                          </div>
+                        </CardContent>
+                      </Card>
+                    );
+                  })}
+                </div>
+              </Element>
             </Grid>
           </Grid>
         </Container>
