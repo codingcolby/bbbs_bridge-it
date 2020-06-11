e4563d1826f9c4d800f454eb62aa9376852c3278
Andrew Nguyen <anguyen1320@gmail.com>
2020-06-11 18:59:51 -0500

    geolib dependencies installed and distance is rendering on littles list as meters

--- src/pages/MapListPage/MapListPage.js
+++ src/pages/MapListPage/MapListPage.js
@@ -2,15 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import Map from "../../components/Map/Map";
-import * as Scroll from "react-scroll";
-import {
-  Link,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller,
-} from "react-scroll";
+import { Element } from "react-scroll";
+import * as geolib from "geolib";
 
 import {
   Button,
@@ -22,8 +15,6 @@ import {
   RadioGroup,
   Grid,
   Slider,
-  withStyles,
-  createStyles,
 } from "@material-ui/core";
 
 const marks = [
@@ -75,8 +66,45 @@ class MapListPage extends Component {
       return item.profile_type === 2;
     });
 
-    console.log(profilesLittlesFilter);
+    const distance = geolib.getPreciseDistance(
+      { latitude: 39.1000564, longitude: -94.5345545 },
+      { latitude: 39.0834741, longitude: -94.5533731 }
+    );
 
+    const miles = geolib.convertDistance(1828, "mi");
+
+    const bigProfile = profilesBigFilter.map((item, index) => {
+      return (
+        <div key={index}>
+          <h1>{item.first_name + " " + item.last_name}</h1>
+          <h3>{"Age: " + item.dob_or_age + " " + "Race: " + item.race}</h3>
+          <h3>{"Address: " + item.address}</h3>
+        </div>
+      );
+    });
+
+    const littlesList = profilesLittlesFilter.map((item, index) => {
+      return (
+        <Card key={index}>
+          <CardContent>
+            <div>
+              <h3>{item.first_name + " " + item.last_name}</h3>
+              <h4>{"Age: " + item.dob_or_age + " " + "Race: " + item.race}</h4>
+              <h4>
+                {geolib.getPreciseDistance(
+                  { latitude: 39.0560072, longitude: -94.5886469 },
+                  { latitude: item.latitude, longitude: item.longitude }
+                )}{" "}
+                miles
+              </h4>
+              <p>{item.summary}</p>
+            </div>
+          </CardContent>
+        </Card>
+      );
+    });
+
+    console.log("DISTANCE:", miles);
     return (
       <div>
         <Container maxWidth={false}>
@@ -112,19 +140,7 @@ class MapListPage extends Component {
               </Button>
             </Grid>
             <Grid item xs={12} sm={4} md={4}>
-              <div>
-                {profilesBigFilter.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      <h1>{item.first_name + " " + item.last_name}</h1>
-                      <h3>
-                        {"Age: " + item.dob_or_age + " " + "Race: " + item.race}
-                      </h3>
-                      <h3>{"Address: " + item.address}</h3>
-                    </div>
-                  );
-                })}
-              </div>
+              <div>{bigProfile}</div>
               <Element
                 name="littles-list"
                 className="element"
@@ -133,29 +149,10 @@ class MapListPage extends Component {
                   height: "540px",
                   overflow: "scroll",
                   marginBottom: "100px",
+                  border: "solid 4px black",
                 }}
               >
-                <div>
-                  {profilesLittlesFilter.map((item, index) => {
-                    return (
-                      <Card key={index}>
-                        <CardContent>
-                          <div>
-                            <h3>{item.first_name + " " + item.last_name}</h3>
-                            <h4>
-                              {"Age: " +
-                                item.dob_or_age +
-                                " " +
-                                "Race: " +
-                                item.race}
-                            </h4>
-                            <p>{item.summary}</p>
-                          </div>
-                        </CardContent>
-                      </Card>
-                    );
-                  })}
-                </div>
+                <div>{littlesList}</div>
               </Element>
             </Grid>
           </Grid>
