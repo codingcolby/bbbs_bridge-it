a86129e5db5af242a16bfd5984ff1ce24b541a42
cchelton <59346941+cchelton@users.noreply.github.com>
2020-06-15 01:53:37 -0500

    Backend -> saga setup / help.
    Created 3 backend routes with colby.
    Routes:
    1. Gets all the current "status" entries
    2. Create a new "status" entry & update comments
    3. Match / No Match an individual & update comments.
    
    This is setup to the point where all the matches are rendering in the matchtablepage.
    
    Co-authored-by: codingcolby <learn2code@respectdesfonds.com>

--- src/redux/sagas/table.saga.js
+++ src/redux/sagas/table.saga.js
@@ -1,39 +1,67 @@
 import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
-import { response } from "express";
 
 function* fetchMatchCandidates() {
-	try {
-		const response = yield axios.get("/api/table");
-		console.log("RESPONSE.DATA", response.data);
+  try {
+    const response = yield axios.get("/api/table");
+    console.log("RESPONSE.DATA", response.data);
 
-		yield put({
-			type: "SET_TABLE",
-			payload: response.data,
-		});
-	} catch (error) {
-		console.log("Match Table candidates get request failed", error);
-	}
+    yield put({
+      type: "SET_TABLE",
+      payload: response.data,
+    });
+  } catch (error) {
+    console.log("Match Table candidates get request failed", error);
+  }
 }
 
-function* upsertStatus(action) {
-	try {
-		const upsertTableData = action.payload;
-		yield axios.put(`/api/table`, action.payload);
-		console.log("RESPONSE.DATA", response.data);
+/**
+ * Creates a new "status" entry
+ * Expected action.payload:
+ *   {
+ *     comment: "the comment on the relationship. Added in the sweetalert on MapListPage",
+ *     review: 1, 2 or 3, this number indicates the type of relationship between the individuals,
+ * 	   big_id: the big's id,
+ * 	   little_id: the little's id,
+ *   }
+ */
+function* createStatusRelationship(action) {
+  try {
+    yield axios.post(`/api/table`, action.payload);
 
-		yield put({
-			type: "SET_TABLE",
-			payload: upsertTableData,
-		});
-	} catch (error) {
-		console.log("upsertStatus request failed", error);
-	}
+    yield put({
+      type: "FETCH_TABLE",
+    });
+  } catch (error) {
+    console.log("createStatusRelationship request failed", error);
+  }
+}
+
+/**
+ * Update match status (true/false) & update any comment changes made.
+ * Expected action.payload:
+ *   {
+ *     big_id: Number,
+ *     little_id: Number,
+ *     comment: Text,
+ *     match: Boolean
+ *   }
+ */
+function* selectMatchStatus(action) {
+  try {
+    yield axios.put(`/api/table/match`); // this is returning id, so you can grab that here if needed
+    yield put({
+      type: "FETCH_TABLE",
+    });
+  } catch (error) {
+    console.log("selectMatchStatus request failed", error);
+  }
 }
 
 function* tableSaga() {
-	yield takeLatest("FETCH_TABLE", fetchMatchCandidates);
-	yield takeLatest("UPSERT_TABLE", upsertStatus);
+  yield takeLatest("FETCH_TABLE", fetchMatchCandidates);
+  yield takeLatest("CREATE_MATCH", createStatusRelationship);
+  yield takeLatest("UPDATE_MATCH_STATUS", selectMatchStatus);
 }
 
 export default tableSaga;
